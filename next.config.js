d84fc76d4b2fa995322ceefea282883ada888fa2
Keith Wagner <244694+kpwags@users.noreply.github.com>
2021-10-03 17:00:27 -0400

    Updated Webpack Config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
@@ -6,9 +8,14 @@ const withMDX = require('@next/mdx')({
 module.exports = {
     target: 'serverless',
     webpack(config) {
+        const webpack = require('webpack');
+
         config.module.rules.push({ test: /\.mdx$/, use: 'raw-loader' });
         config.module.rules.push({ test: /\.yml$/, use: 'raw-loader' });
-        config.module.externals.push('canvas');
+
+        // eslint-disable-next-line no-param-reassign
+        config.plugins = config.plugins || [];
+        config.plugins.push(new webpack.IgnorePlugin(/canvas/, /jsdom$/));
         return config;
     },
 };
