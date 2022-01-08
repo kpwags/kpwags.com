a24f06aa019f1c44289cc0aef022d45a5e15e36f
Keith Wagner <244694+kpwags@users.noreply.github.com>
2022-01-07 20:38:37 -0500

    Added Post Meta Data to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,11 +29,33 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>Keith Wagner</title>
+                <title>{pageProps.post ? `${pageProps.post.title} - Keith Wagner` : 'Keith Wagner'}</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <meta property="og:locale" content="en_US" />
                 <meta property="og:site_name" content="Keith Wagner" />
                 <meta name="twitter:creator" content="@kpwags" />
+                {pageProps.post ? (
+                    <>
+                        <meta name="description" content={pageProps.post.description || ''} />
+                        <meta property="og:type" content="article" />
+                        <meta property="og:title" content={pageProps.post.title} />
+                        <meta property="og:description" content={pageProps.post.description || ''} />
+                        <meta property="og:url" content={`https://kpwags.com${pageProps.post.url}`} />
+                        {pageProps.post.socialImageUrl && <meta property="og:image" content={`https://kpwags.com/images/posts/${pageProps.post.socialImageUrl}`} />}
+                        {pageProps.post.socialImageWidth && <meta property="og:image:width" content={pageProps.post.socialImageWidth.toString()} />}
+                        {pageProps.post.socialImageHeight && <meta property="og:image:height" content={pageProps.post.socialImageHeight.toString()} />}
+                        {pageProps.post.socialImageUrl && (
+                            <>
+                                <meta name="twitter:card" content="summary" />
+                                <meta name="twitter:site" content="@kpwags" />
+                                <meta name="twitter:title" content={pageProps.post.title} />
+                                <meta name="twitter:description" content={pageProps.post.description} />
+                                <meta name="twitter:image" content={`https://kpwags.com/images/posts/${pageProps.post.socialImageUrl}`} />
+                            </>
+                        )}
+                        {pageProps.post.hasEmbeddedTweet && <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />}
+                    </>
+                ) : null}
             </Head>
             {themeLoaded && (
                 <BlogContext.Provider
--- pages/posts/[year]/[month]/[day]/[id].tsx
+++ pages/posts/[year]/[month]/[day]/[id].tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { BlogPost } from '@models/blogPost';
 import BlogEntry from '@components/BlogEntry';
 import { GetStaticProps, GetStaticPaths } from 'next';
@@ -36,27 +35,6 @@ interface PostProps {
 
 const Post = ({ post }: PostProps): JSX.Element => (
     <>
-        <Head>
-            <title>{`${post.title} - Keith Wagner`}</title>
-            <meta name="description" content={post.description || ''} />
-            <meta property="og:type" content="article" />
-            <meta property="og:title" content={post.title} />
-            <meta property="og:description" content={post.description || ''} />
-            <meta property="og:url" content={`https://kpwags.com${post.url}`} />
-            {post.socialImageUrl && <meta property="og:image" content={`https://kpwags.com/images/posts/${post.socialImageUrl}`} />}
-            {post.socialImageWidth && <meta property="og:image:width" content={post.socialImageWidth.toString()} />}
-            {post.socialImageHeight && <meta property="og:image:height" content={post.socialImageHeight.toString()} />}
-            {post.socialImageUrl && (
-                <>
-                    <meta name="twitter:card" content="summary" />
-                    <meta name="twitter:site" content="@kpwags" />
-                    <meta name="twitter:title" content={post.title} />
-                    <meta name="twitter:description" content={post.description} />
-                    <meta name="twitter:image" content={`https://kpwags.com/images/posts/${post.socialImageUrl}`} />
-                </>
-            )}
-            {post.hasEmbeddedTweet && <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />}
-        </Head>
         <RssFeeds />
         <PageScrollIndicator />
         <main>
