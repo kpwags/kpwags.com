253ed19d84a6da0f51cc2f81743f5e928ab11781
Keith Wagner <github@kpwags.com>
2022-08-29 18:25:55 -0400

    Updated Now Page and Updated Goal Progress

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -80,7 +80,7 @@ const Welcome = (): JSX.Element => (
             <Link href="/work"><a>Work</a></Link>
             <Link href="/uses"><a>Uses</a></Link>
             <Link href="/contact"><a>Contact</a></Link>
-            <Link href="/current"><a>What I&apos;m Up To</a></Link>
+            <Link href="/now"><a>Now</a></Link>
             <Link href="/bookshelf"><a>Bookshelf</a></Link>
             <Link href="/likes"><a>Likes</a></Link>
         </div>
