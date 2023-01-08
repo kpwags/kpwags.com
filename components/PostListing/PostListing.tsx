c67d0f05bb724d51431945e6e264303167a9098c
Keith Wagner <244694+kpwags@users.noreply.github.com>
2023-01-08 15:04:51 -0500

    Changed Blog List to CSS Modules
    Changed the blog listing to use CSS Modules

--- components/PostListing/PostListing.tsx
+++ components/PostListing/PostListing.tsx
@@ -1,53 +1,8 @@
-import styled from 'styled-components';
 import Link from 'next/link';
 import { formatDate } from '@lib/utilities';
 import { BlogPost } from '@models/blogPost';
 
-const Post = styled.article`
-    list-style-type: none;
-    margin: 40px 0;
-
-    h2 {
-        color: var(--primary-color-1);
-        font-size: 2rem;
-        font-weight: 500;
-    }
-
-    .metadata {
-        margin: 5px 0;
-        font-style: italic;
-        color: var(--meta);
-        font-size: 1.1rem
-    }
-
-    p {
-        font-weight: 300;
-        font-size: 1.25rem;
-    }
-
-    &.boxed {
-        border: 1px solid var(--meta);
-        padding: 1rem;
-        box-shadow: var(--box-shadow);
-
-        p {
-            margin: 0;
-        }
-    }
-`;
-
-const Tags = styled.ul`
-    display: inline-block;
-    margin: 0;
-    padding: 0;
-
-    li {
-        margin: 0 0.8rem 0 0;
-        list-item-type: none;
-        display: inline-block;
-        font-size: 1.1rem;
-    }
-`;
+import styles from './PostListing.module.css';
 
 interface PostListingProps {
     post: BlogPost
@@ -60,7 +15,7 @@ const PostListing = ({
     showBorder = false,
     showTags = false,
 }: PostListingProps): JSX.Element => (
-    <Post key={post.id} className={showBorder ? 'boxed' : ''}>
+    <article key={post.id} className={showBorder ? styles.boxedArticle : styles.article}>
         <h2><Link href={post.url}><a>{post.title}</a></Link></h2>
         <div className="metadata">
             {formatDate(post.date)}
@@ -68,16 +23,16 @@ const PostListing = ({
         </div>
 
         {showTags ? (
-            <Tags>
+            <ul className={styles.tags}>
                 {post.tags.map((t) => (
                     <li key={t.url}><Link href={`/tag/${t.url}`}><a>#{t.name}</a></Link></li>
                 ))}
-            </Tags>
+            </ul>
         ) : null}
 
         {/* eslint-disable-next-line react/no-danger */}
         {post.excerpt !== null ? <p dangerouslySetInnerHTML={{ __html: post.excerpt || 'No content found' }} /> : null}
-    </Post>
+    </article>
 );
 
 export default PostListing;
