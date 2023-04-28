24c5098f086fc0fb657cae3e4c3f341440ab9554
Keith Wagner <244694+kpwags@users.noreply.github.com>
2023-04-27 20:41:47 -0400

    Changing fallback to “blocking”

--- pages/photoblog/[key].tsx
+++ pages/photoblog/[key].tsx
@@ -11,7 +11,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: 'blocking',
     };
 };
 
--- pages/tag/[tag]/index.tsx
+++ pages/tag/[tag]/index.tsx
@@ -12,7 +12,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: 'blocking',
     };
 };
 
