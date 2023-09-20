8bfb3f3c8d296c26fd34d9f759bfe47a68cd2f01
Keith Wagner <244694+kpwags@users.noreply.github.com>
2023-09-20 18:48:35 -0400

    Added Landing/Home Page
    Added and styled the landing and home page
    
    Closes #507

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { getAllPosts } from '@lib/posts';
 import { GetStaticProps } from 'next';
 import Welcome from '@components/Welcome';
@@ -7,10 +8,16 @@ import RssFeeds from '@components/RssFeeds';
 import { generateAllRssFeeds } from '@lib/rss';
 import { getAllReadingLogs } from '@lib/readinglog';
 import { ReadingLog } from '@models/ReadingLog';
+import { getAllBookNotes } from '@lib/books';
+import { BookNote } from '@models/BookNote';
+import BookNoteListing from '@components/BookNoteListing';
+
+import styles from '@css/Landing.module.css';
 
 export const getStaticProps: GetStaticProps = async () => {
     const blogPosts = getAllPosts();
     const readingLogs = getAllReadingLogs();
+    const bookNotes = getAllBookNotes();
 
     await generateAllRssFeeds();
 
@@ -18,15 +25,17 @@ export const getStaticProps: GetStaticProps = async () => {
         props: {
             posts: blogPosts.length <= 6 ? blogPosts : blogPosts.slice(0, 6),
             readingLogs: readingLogs.slice(0, 3),
+            bookNote: bookNotes[0],
         },
     };
 };
 interface HomeProps {
     posts: BlogPost[];
     readingLogs: ReadingLog[];
+    bookNote: BookNote;
 }
 
-const Home = ({ posts, readingLogs }: HomeProps): JSX.Element => (
+const Home = ({ posts, readingLogs, bookNote }: HomeProps): JSX.Element => (
     <>
         <RssFeeds />
         <Welcome />
@@ -42,6 +51,12 @@ const Home = ({ posts, readingLogs }: HomeProps): JSX.Element => (
             posts={readingLogs.map((p) => ({ title: p.title.replace('Reading Log - ', ''), url: p.url }))}
             viewMoreLink="/reading-logs"
         />
+
+        <h2 className={styles.latestBookNote}>Latest Book Note</h2>
+        <BookNoteListing bookNote={bookNote} />
+        <div className={styles.viewMore}>
+            <Link href="/book-notes">View More</Link>
+        </div>
     </>
 );
 
